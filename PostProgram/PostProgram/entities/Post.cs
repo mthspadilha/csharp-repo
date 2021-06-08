c289d24a7a0f91cc4c2bfe280d053b5fba671a28
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-08 17:07:50 -0300

    new program

---
+++ PostProgram/PostProgram/entities/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostProgram.entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Tittle { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+        //O atributo list<comments> não entra no construtor geralmenteo por ser um pra muitos 
+        public Post(DateTime moment, string tittle, string content, int likes)
+        {
+            Moment = moment;
+            Tittle = tittle;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+    }
+}
