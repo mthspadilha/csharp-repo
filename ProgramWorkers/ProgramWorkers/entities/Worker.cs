b07de1b00801a0b6cc02381fc3846448858bfc3e
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-08 13:33:19 -0300

    Income method

--- ProgramWorkers/ProgramWorkers/entities/Worker.cs
+++ ProgramWorkers/ProgramWorkers/entities/Worker.cs
@@ -31,5 +31,25 @@ namespace ProgramWorkers.entities
             Contracts.Add(contract);
         }
 
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum = sum + contract.TotalValue();
+                     
+                }
+            }
+            return sum;
+
+        }
     }
 }
